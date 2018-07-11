12be50217c22e484c82687efec2415ad69a85084
S337544818 <337544818@qq.com>
2018-07-11 22:53:09 +0800

    The 4 days

--- String类及函数实现/strcpy实现.cpp
+++ String类及函数实现/strcpy实现.cpp
@@ -4,11 +4,12 @@
 
 using namespace std;
 
-char *strcpy(char *strDest, const char *strSrc)
+char *strcpy(char *strDes, const char *string)
 {
-	assert((strDest != NULL) && (strSrc != NULL));
-	char *address = strDest;
-	while (*strSrc != '\0')
-		*strDest++ = *strSrc++;
-	return address;
-}
+	if (string == NULL&&strDes == NULL)
+		return NULL;
+	char* address = strDes;
+	while (*string != '\0')
+		*(strDes++) = *(string++);
+	*strDes = '\0';//一定注意最后结束时一定要加一个\0结尾；
+	return address;//返回保存的strDes首地址;
