1d1e4522b1a1ac2dc15854406b5a9dbc3731bc2f
S337544818 <337544818@qq.com>
2018-07-12 22:51:07 +0800

    The 5 days

---
+++ 位域/位域.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include <iostream>
+struct S {
+	// 三位无符号位域，
+	// 允许值为 0...7
+	unsigned int b : 3;.
+	//int b:3; 可拥有值范围 0..7 或 -4..3 。
+	unsigned int a = 0;
+};
+int main()
+{
+	S s = { 7,1};//列表初始化
+	++s.b; // 无符号上溢（保证回卷）
+	std::cout << s.b << '\n'; // 输出： 0
+	std::cout << s.a << "\n";//输出：1
+}
+详见 https://zh.cppreference.com/w/cpp/language/bit_field
---
+++ 类/局部类/局部类.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+int main()
+{
+	std::vector<int> v{ 1,2,3 };
+	struct Local {
+		bool operator()(int n, int m) {
+			return n > m;
+		}
+	};
+	std::sort(v.begin(), v.end(), Local()); // C++11 起 局部类
+	//等价于std::sort(v.begin(), v.end(), [](int n, int m) {return n > m; });
+	for (int n : v) std::cout << n << ' ';
+}
+
+https://zh.cppreference.com/w/cpp/language/class
+详见
