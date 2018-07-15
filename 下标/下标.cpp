473075743d5c93bc7d422d69a93994d800796a9a
S337544818 <337544818@qq.com>
2018-07-15 21:58:41 +0800

    The 7 days

---
+++ 下标/下标.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include <iostream>  
+
+using namespace std;
+class IntVector {
+public:
+	IntVector(int cElements);
+	~IntVector() { delete[] _iElements; }
+	int& operator[](int nSubscript);
+private:
+	int *_iElements;
+	int _iUpperBound;
+};
+
+// Construct an IntVector.  
+IntVector::IntVector(int cElements) {
+	_iElements = new int[cElements];
+	_iUpperBound = cElements;
+}
+
+// Subscript operator for IntVector.  
+int& IntVector::operator[](int nSubscript) {
+	static int iErr = -1;
+
+	if (nSubscript >= 0 && nSubscript < _iUpperBound)
+		return _iElements[nSubscript];
+	else {
+		clog << "Array bounds violation." << endl;
+		return iErr;
+	}
+}
+
+// Test the IntVector class.  
+int main() {
+	IntVector v(10);
+	int i;
+
+	for (i = 0; i <= 10; ++i)
+		v[i] = i;
+
+	v[3] = v[9];
+
+	for (i = 0; i <= 10; ++i)
+		cout << "Element: [" << i << "] = " << v[i] << endl;
+}
+
+
+详见：https://msdn.microsoft.com/zh-cn/library/1bhdy722.aspx
---
+++ 针对函数的依赖于参数的名称 (Koenig) 查找/针对函数的依赖于参数的名称 (Koenig) 查找.cpp
@@ -0,0 +1,18 @@
+详见：https://msdn.microsoft.com/zh-cn/library/60bx1ys7.aspxs
+// argument_dependent_name_koenig_lookup_on_functions.cpp  
+namespace A  
+{  
+   struct X  
+   {  
+   };  
+   void f(const X&)  
+   {  
+   }  
+}  
+int main()  
+{  
+// The compiler finds A::f() in namespace A, which is where   
+// the type of argument x is defined. The type of x is A::X.  
+   A::X x;  
+   f(x);     
+}  
